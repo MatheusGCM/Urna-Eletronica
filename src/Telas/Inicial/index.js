fcfc8208793afeb9563eae032576e39694b65800
MatheusGCM <nexteu10@outlook.com>
2022-05-29 22:44:21 -0300

    Atualização

--- src/Telas/Inicial/index.js
+++ src/Telas/Inicial/index.js
@@ -4,39 +4,42 @@ import style from './style';
 function Inicial() {
   return (
     <View style={style.background}>
-       
-        <View style={style.boxImage}>
-          <Image  style={style.image} source={require('../../assets/Urna.png')} />
-        </View>
+      <View style={style.boxImage}>
+        <Image style={style.image} source={require('../../assets/Urna.png')} />
+      </View>
 
-        <View style={style.boxTitulo}>
-          <Text style={style.titulo}>Bem-Vindo a Urna Eletronica</Text>
-      
-          <Text style={style.subtitulo}>Eleição 2022</Text>
-          
-        </View>
+      <View style={style.boxTitulo}>
+        <Text style={style.titulo}>Bem-Vindo a Urna Eletronica</Text>
 
-        <View style={style.boxTexto}>
-          <Text style={style.textoDescricao}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            faucibus massa id augue egestas, non rhoncus risus fringilla. Sed
-            convallis laoreet ante non porttitor. Sed mattis, nunc feugiat
-            consequat feugiat, urna nulla faucibus ipsum, et blandit neque diam
-            vitae augue. Suspendisse quis pulvinar tortor. Vestibulum ex orci,
-            scelerisque vitae justo at, cursus sollicitudin tortor. Sed ut
-            feugiat mauris. Vivamus blandit orci nec felis accumsan, quis
-            interdum .
-          </Text>
-          <View style={{ width: '100%',flexDirection:'row',justifyContent:'space-evenly'}}>
-          <Image style={{width:120, height:50,resizeMode:'contain'}} source={require('../../assets/GovernoF.png')} />
-          <Image style={{width:120, height:40}} source={require('../../assets/Brasil.png')} />
+        <Text style={style.subtitulo}>Eleições 2022</Text>
+      </View>
+
+      <View style={style.boxTexto}>
+        <Text style={style.textoDescricao}>
+          Numa democracia, como ocorre no Brasil, as eleições são de fundamental
+          importância, além de representar um ato de cidadania. Possibilitam a
+          escolha de representantes e governantes que fazem e executam leis que
+          interferem diretamente em nossas vidas.
+        </Text>
+        <View>
+          <View style={style.logos}>
+            <View>
+              <Image
+                style={{width: 170, height: 70, resizeMode: 'contain'}}
+                source={require('../../assets/GovernoF.png')}
+              />
+            </View>
+            <View>
+              <Image
+                style={{width: 170, height: 70, resizeMode: 'contain'}}
+                source={require('../../assets/Brasil.png')}
+              />
+            </View>
           </View>
-
         </View>
-     
+      </View>
     </View>
   );
 }
-
 
 export default Inicial;
--- src/mocks/Candidato.js
+++ src/mocks/Candidato.js
@@ -1,45 +1,44 @@
-
 const Candidato = [
-     {
-        nome: "Kleber",
-        numero: '12',
-        partido: "PLL",
-        slogan: "Comigo o Brasil vai mudar",
-        img: require("../assets/candidatos/Kleber.png"),
-        votacao: 0
-    },
-     {
-        nome: "Fabio",
-        numero: '13',
-        partido: "PQQ",
-        slogan: "Comigo é diferente",
-        img: require("../assets/candidatos/Fabio.png"),
-        votacao: 0
-    },
-     {
-        nome: "Matheus",
-        numero: '42',
-        partido: "Partido Novo Porém Antigo(PNPA)",
-        slogan: "Para o fim da corrupção e inicio de mais, vote PNPA",
-        img: require("../assets/candidatos/Matheus.jpeg"),
-        votacao: 0
-    },
-     {
-        nome: "Faustino Neto",
-        numero: '17',
-        partido: "Partido Liberal(PL)",
-        slogan: "Vote em faustino neto, com ele o crescimenro é certo",
-        img: require("../assets/candidatos/Faustino.jpeg"),
-        votacao: 0
-    },
-    {
-        nome: "Luiza Chachapoya",
-        numero: '97',
-        partido: "Partido dos Nativos das Américas(PNA)",
-        slogan: "Pela luta dos direitos dos povos indígenas de todo o Brasil",
-        img: require("../assets/candidatos/Luiza.jpeg"),
-        votacao: 0
-    }
-]
+  {
+    nome: 'Kleber',
+    numero: '12',
+    partido: 'PLL',
+    slogan: 'Comigo o Brasil vai mudar.',
+    img: require('../assets/candidatos/Kleber.png'),
+    votacao: 0,
+  },
+  {
+    nome: 'Fabio',
+    numero: '13',
+    partido: 'PQQ',
+    slogan: 'Comigo é diferente.',
+    img: require('../assets/candidatos/Fabio.png'),
+    votacao: 0,
+  },
+  {
+    nome: 'Matheus',
+    numero: '42',
+    partido: 'Partido Novo Porém Antigo(PNPA)',
+    slogan: 'Para o fim da corrupção e inicio de mais, vote PNPA.',
+    img: require('../assets/candidatos/Matheus.jpeg'),
+    votacao: 0,
+  },
+  {
+    nome: 'Faustino Neto',
+    numero: '17',
+    partido: 'Partido Liberal(PL)',
+    slogan: 'Vote em faustino neto, com ele o crescimenro é certo.',
+    img: require('../assets/candidatos/Faustino.jpeg'),
+    votacao: 0,
+  },
+  {
+    nome: 'Luiza Chachapoya',
+    numero: '97',
+    partido: 'Partido dos Nativos das Américas(PNA)',
+    slogan: 'Pela luta dos direitos dos povos indígenas de todo o Brasil.',
+    img: require('../assets/candidatos/Luiza.jpeg'),
+    votacao: 0,
+  },
+];
 
-export default Candidato
+export default Candidato;
